3ba96fcc203b1f671b07c81d35b303dae8dca081
Gene Kogan <kogan.gene@gmail.com>
2014-05-29 01:36:20 -0400

    initial commit

--- example_leap/src/ofApp.h
+++ example_leap/src/ofApp.h
@@ -8,7 +8,7 @@ class ofApp : public ofBaseApp{
 public:
     void setup();
     void update();
-    void draw();
+    void draw(); 
 	
     void keyPressed  (int key);
     void keyReleased(int key);
